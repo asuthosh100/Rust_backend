04957427eab8474e9b5022ec13b46bbdc76197f4
aa69 <aa69@illinois.edu.com>
2025-10-09 17:29:48 -0400

    Borrowing concept done

--- chapter2/material.rs
+++ chapter2/material.rs
@@ -13,3 +13,25 @@ fn main() {
 // In the above example, when we assign s to t, the ownership is moved not copied. 
 
 // Prevents Use After Free Errors at Compile Time, doublefree errors, dangling pointers. 
+
+//----------------------------------------Borrowing--------------------------------------------------------------------------------
+// We can use references to let multiple variables access the same data without taking ownership of it. 
+// Rust allows you to borrow data immutably or mutably, but with strict rules. 
+
+// Immutable borrow :
+
+fn print_string(s: &String) {
+    println!("{}", s);
+}
+
+// Here any number of immutable references are allowed. (SIMILAR TO READ ONLY ACCESS)
+
+// Mutable borrow :
+
+fn print_string(s : &mut String) {
+    s.push_str(", world!");
+    println!("{}", s);
+}
+// Here only one mutable reference is allowed at a time. (SIMILAR TO READ AND WRITE ACCESS) 
+
+//Borrowing feature is helpful when it comes to multiple reads and exclusive writes without requiring locks and semaphores.
