7d482bbc51db672d487293ec5a34fd28cdbca343
aa69 <aa69@illinois.edu.com>
2025-10-09 17:20:28 -0400

    Rust Refresher

--- chapter2/material.rs
+++ chapter2/material.rs
@@ -2,3 +2,14 @@
     // 1. Each value in Rust has a variable that’s called its owner.
     // 2. There can only be one owner at a time. 
     // 3. When the owner goes out of scope, the value will be dropped.
+
+
+fn main() {
+    let s = String::from("hello");
+    let t = s; 
+   // println!("{}", s); // error: borrow of moved value: `s`
+}
+
+// In the above example, when we assign s to t, the ownership is moved not copied. 
+
+// Prevents Use After Free Errors at Compile Time, doublefree errors, dangling pointers. 
