a19cc6ff99e096ff343328f1ad6a7048780178c8
Asuthosh Anandaram <asuthoshanandaram@gmail.com>
2025-10-11 18:11:47 -0400

    Logger Thread Defined

--- chapter2/hello_world/src/main.rs
+++ chapter2/hello_world/src/main.rs
@@ -1,9 +1,22 @@
 // --------------------------------STEP 1 : Project Setup--------------------------------
 
-use std::sync::mpsc;
-use std::sync:Arc; 
+use std::sync::mpsc; // multiple producer, single consumer
+use std::sync:Arc; // atomic reference counting
 use std::thread;
 use std::time::Duration;
+
+//--------------------------------STEP 2 : Define the logger thread-------------------------------
+
+// parameter is a receiver that gets messages from other threads
+fn start_logger(rx: mpsc::Receiver<String>) { // example of how such a reciver is created => let (tx, rx) = mpsc::channel::<String>();
+    thread::spawn(move || { // thread::spawn creates a new thread and move keyword transfers the ownership of rx to the new thread
+        for msg in rx {
+            println!("Log: {}", msg);
+        }
+    });
+}
+
+
 
 fn main() {
     println!("Hello, world!");
