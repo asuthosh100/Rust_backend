c39ee4830cf1c1d0337c1d63064574948add7bc2
Asuthosh Anandaram <asuthoshanandaram@gmail.com>
2025-10-11 21:08:07 -0400

    file reader module completed

--- chapter2/file_reader_module/src/main.rs
+++ chapter2/file_reader_module/src/main.rs
@@ -1,16 +1,47 @@
-// use std::io::Error; 
+// u
 
 // fn read_lines(filename: &str)-> Result<Vec<String>, Error>{
 
 // }
 
 use std::fs::File; // for file handling (Open, create, close)
-use std::io::Write;  
+// use std::io::Write;  
+// use std::io::Read; 
+use std::io::Error; 
+use std::io::{self, BufRead, BufReader};
 
-fn main() {
-    let mut file = File::create("example.txt").expect("Could not create file"); // create a new price
+fn read_lines(filename: &str) -> Result<Vec<String>, Error> {
 
-    file.write_all(b"Hello, world!").expect("Could not write to file"); // write some data to the file
+    let file = File::open(filename)?; 
+    let reader = BufReader::new(file); 
 
-    println!("File created and data written successfully.");
+    let mut lines = Vec::new(); 
+    for line in reader.lines() {
+        //println!("Line: {}", line?);
+        lines.push(line?); 
+    }
+
+    Ok(lines)
+}
+
+// fn main() -> io::Result<()> {
+//     let file = File::open("example.txt")?;
+//     let reader = BufReader::new(file);
+
+//     for line in reader.lines() {
+//         println!("Line: {}", line?);
+//     }
+
+//     Ok(())
+// }
+
+
+fn main() -> io::Result<()> {
+    let lines = read_lines("example.txt")?;
+
+    for (i, line) in lines.iter().enumerate() { // loop over items without taking ownership
+        println!("Line {}: {}", i + 1, line);
+    }
+
+    Ok(())
 }
