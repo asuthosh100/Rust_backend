47a0d4d2b52cf821055fe28e15517d1486af7e6f
Asuthosh Anandaram <asuthoshanandaram@gmail.com>
2025-10-11 22:27:30 -0400

    traits

--- chapter2/trait_based_api/src/main.rs
+++ chapter2/trait_based_api/src/main.rs
@@ -1,3 +1,65 @@
+
+// trait Animal {
+//     fn speak(&self);
+// }
+
+// struct Dog;
+// struct Cat;
+
+// impl Animal for Dog {
+//     fn speak(&self) {
+//         println!("Woof!");
+//     }
+// }
+
+// impl Animal for Cat {
+//     fn speak(&self) {
+//         println!("Meow!");
+//     }
+// }
+
+// fn make_it_speak(animal: &dyn Animal) {
+//     animal.speak();
+// }
+
+// fn main() {
+//     let dog = Dog;
+//     let cat = Cat;
+
+//     make_it_speak(&dog);
+//     make_it_speak(&cat);
+// }
+
+// trait Animal	Defines a common behavior — “things that can speak.”
+// impl Animal for Dog	Dog knows how to speak.
+// impl Animal for Cat	Cat also knows how to speak.
+// fn make_it_speak(animal: &dyn Animal)	Function that can work with any Animal.
+// make_it_speak(&dog) and make_it_speak(&cat)	Same function, different behavior.
+// So the code doesn’t care what kind of animal it is —
+// it just knows the animal can .speak().
+// That’s polymorphism!
+
+
+trait Metrics {
+    fn record(&self, metric: &str, value: f64);
+}
+
+struct ConsoleMetrics; 
+
+impl Metrics for ConsoleMetrics {
+    fn record(&self, metric: &str, value: f64) {
+
+    }
+}
+
+
+fn run_metrics_demo(metrics : &dyn Metrics) {
+    metrics.record(); 
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let conmet = ConsoleMetrics; 
+
+    run_metrics_demo(&conmet);
 }
