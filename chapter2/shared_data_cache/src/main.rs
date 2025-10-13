7f6464352dd853429d3f72328adb784abe19ce82
Asuthosh Anandaram <asuthoshanandaram@gmail.com>
2025-10-13 00:53:31 -0400

    building blocks

--- chapter2/shared_data_cache/src/main.rs
+++ chapter2/shared_data_cache/src/main.rs
@@ -1,3 +1,13 @@
+// use RWlock for multiple readers or single writer
+// use threads for readers and writers
+// use Arc to share safety across threads
+
+// for the data cache we will use a hashmap 
+
+use std::collections::Hashmap;
+use std::sync::{Arc, RwLock};
+
 fn main() {
-    println!("Hello, world!");
+    let data : Hashmap<String, String> = Hashmap::new(); 
+    let shared_data = Arc::new(RwLock::new(data)); 
 }
